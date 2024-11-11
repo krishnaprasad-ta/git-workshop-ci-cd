c745fea27a043f9c6ecb1e02ebcbcccc2b7cc649
krishnaprasad-ta <krishnaprasad.yv@tigeranalytics.com>
2024-11-11 11:51:41 +0530

    Deploying build

---
+++ src/my_package/simple_functions.py
@@ -0,0 +1,37 @@
+# Custom python functions
+
+
+def double_number(a):
+    print(f"value before double_number(): {a}")
+    result = a + a
+    print(f"value after double_number(): {result}")
+
+
+"""
+    Doubles the input number and prints
+    the value before and after the operation.
+
+    Parameters:
+    a (int or float): The number to be doubled.
+
+    Returns:
+    None
+"""
+
+
+def square_number(a):
+    print(f"value before square_number(): {a}")
+    result = a * a
+    print(f"value after square_number(): {result}")
+
+
+"""
+    Squares the input number and prints the
+    value before and after the operation.
+
+    Parameters:
+    a (int or float): The number to be squared.
+
+    Returns:
+    None
+"""
